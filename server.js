1e0204ca71a40ee3b16224eecff153014fc253cd
pinglu85 <luping8576@hotmail.com>
2020-09-17 21:26:49 +0200

    add CSP img-src directive

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(
           'code.jquery.com',
         ],
         styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", 'cdn.gomix.com'],
       },
     },
     frameguard: {
